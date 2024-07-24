a62821e82773f6a5d8078c4e03fbb01972ec3b0d
Maximilian Fabi <max.fa@outlook.de>
2024-07-24 14:03:48 +0200

    an error occured, that i need to save it as .psx file (which i did..). now changed the document save function

--- pipeline.py
+++ pipeline.py
@@ -128,7 +128,8 @@ class MetashapeProcessor:
 
         # Define the path for the project file
         project_path = os.path.join(folder_path, "project.psx")
-        project = MetashapeProject(project_path)
+        project = Metashape.Document()
+        project.save(project_path)
 
         # Locate the "photos" directory within the "_unprocessed" folder
         photos_dir = os.path.join(folder_path, "photos")
