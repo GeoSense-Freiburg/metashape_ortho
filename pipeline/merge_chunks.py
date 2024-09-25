39f88fca6e17bdab96e4055326bd4fec58a70b5b
carrapatofa <github@alias.maximilianfabi.de>
2024-09-25 06:57:17 +0000

    merge chunks from biologie (no normal task, therefore not saving, just commiting)

--- pipeline/merge_chunks.py
+++ pipeline/merge_chunks.py
@@ -41,39 +41,48 @@ move_all_files(project_files_path, tmp_project_files_path)
 
 # Define the path for the project file
 project = MetashapeProject(tmp_project_path)
+export_folder = "/mnt/data/tmp_orthos/biologie/"
 
-# Merging chunks
-merged_chunk = project.mergeChunks(chunks=project.chunks, merge_dense_clouds=True, merge_models=True, merge_markers=True)
+# the magic
+chunks_to_process_again = [63, 64, 65, 66, 67, 68, 69, 70, 71, 72]
+for chunk_key in chunks_to_process_again:
+    # Search for the chunk with the specified key
+    for chunk in doc.chunks:
+        if chunk.key == chunk_key:
+            project.doc.chunk = chunk  # Set the found chunk as the active chunk
+            print(f"Active chunk set to: {chunk.label} (Key: {chunk.key})")
 
-processor = MetashapeChunkProcessor(merged_chunk)
+            processor = MetashapeChunkProcessor(chunk)
+            #
+            processor.build_depth_maps()
+            project.save()
 
-#
-processor.build_depth_maps()
-project.save()
+            #
+            processor.build_model()
+            project.save()
 
-#
-processor.build_model()
-project.save()
+            has_transform = chunk.transform.scale and chunk.transform.rotation and chunk.transform.translation
 
-has_transform = chunk.transform.scale and chunk.transform.rotation and chunk.transform.translation
+            if has_transform:
+                #
+                processor.build_point_cloud()
+                project.save()
 
-if has_transform:
-    #
-    processor.build_point_cloud()
-    project.save()
+                #
+                processor.smooth_model()
+                project.save()
 
-    #
-    processor.smooth_model()
-    project.save()
+                #
+                processor.build_orthomosaic()
+                project.save()
 
-    #
-    processor.build_orthomosaic()
-    project.save()
+            # Export orthomosaic for each channel (RGB, NIR, RE, R, G)
+            #
+            processor.export_raster(export_folder)
+            project.save()
+            print("Orthomosaic created successfully!")
 
-# Export orthomosaic for each channel (RGB, NIR, RE, R, G)
-#
-export_folder = os.path.dirname(tmp_project_path)
-processor.export_raster(export_folder)
-project.save()
 
-print("Orthomosaic created successfully!")
+# move project.psx and project.files
+
+move_all_files("/mnt/data/tmp_orthos/biologie/", "/mnt/gsdata/projects/other/Biologie_Konrad/multispektral_processed/")
